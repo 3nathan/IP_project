d4837f39c48ff2050df1af01cbd84a994eaeb3ea
Ben Marconi <54138902+ben-marconi@users.noreply.github.com>
2023-03-06 16:23:58 +0000

    Update server.py

--- game/server.py
+++ game/server.py
@@ -1,6 +1,8 @@
 import socket
 import thread
 import pickle
+import boto3
+from boto3.dynamodb.conditions import Key
 #install these libraries on server !!!!!!!!!!!!!
 
 
@@ -19,31 +21,87 @@ server_socket.listen(2)
 #ready message
 print('Server running on port ', port)
 
+### at start of game
+# start game when both clients have pressed start
+# take in song name and username
+# start loop
+# initialize dictionary
+
+### every iteration
+# update dictionary
+# send dictionary information to each client
+
+### at the end of loop
+# store entries in database
+# return to client top 5 scores for the song
+
+
 
 
 #keep track of clients
-client_sockets = []
-scores = dict()
+client_sockets = set()
+scores = {"player0": 0, "player1": 0}
 
+def store_score(song, user, score, dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb', region_name='eu-west-2')
+
+    table = dynamodb.Table('Scores')
+    response = table.put_item(
+       Item={
+            'song': song,
+            'user': user,
+            'info': {
+                'score': score
+            }
+        }
+    )
+    return response
+
+
+def get_scores(song, dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb', region_name='eu-west-2')
+
+    table = dynamodb.Table('Scores')
+    response = table.query(
+        KeyConditionExpression=Key('song').eq(song)
+    )
+    scores = []
+    for item in response['Items']:
+        scores.append((item['user', item['info']['score']]))
+    upto = min(5, len(scores))
+    return scores.sort(by = lambda x: x[1])[:upto]
+
+
+# TODO: send index of arrow hit to both players
 def client_thread(clientsocket, addr):
     while True:
         data = clientsocket.recv(1024).decode()
-        #handle data here
-        if data:
-            scores[addr] = scores.get(addr, 0) + 1
-
-        #update database
-
-
-        # send the data to the client
-        for client in client_sockets:
-            pass
-        connection_socket.send(cmsg.encode())
+        user, index = data[0], data[1]
+        if user == "start":
+            song = data[1]
+            scores["player0"], scores["player1"] = 0, 0
+            continue
+        #if game has finished
+        elif user == "stop":
+            #insert score into database
+            response = store_score(song, user, scores[user], 0)
+            #retrieve top 5 highest scores for that song and send it to every client
+            top_scores = get_scores(song)
+            for client in client_sockets:
+                client.sendall(top_scores)
+            
+        else:
+            scores[user] += 1
+            #update clients with scores
+            for client in client_sockets:
+                client.sendall(scores)
     clientsocket.close()
 
 
 #Now the main server loop 
 while True:
     connection_socket, caddr = server_socket.accept() 
-    print("Connected to:", caddr) #Ana
+    client_sockets.add(connection_socket)
     thread.start_new_thread(client_thread, (connection_socket, caddr))
