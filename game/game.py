a8d8316f95054af6f8ec1863e6187af68311d8ad
Nathan Dalton <nd221@ic.ac.uk>
2023-03-11 23:37:57 +0000

    tidy game.py

--- game/game.py
+++ game/game.py
@@ -14,12 +14,6 @@ class Game():
         self.stateStack = []
         self.__loadStates()
 
-    # this should be 5-10 seconds after the song ends
-    # not after the las arrow has passed
-    #def __isGameRunning(self):
-    #    lastArrow = self.arrows[len(self.arrows)-1]
-    #    if not lastArrow.isAlive():
-    #        self.running = 0
     def __updateEvents(self):
         for event in pygame.event.get():
             if event.type == KEYDOWN:
@@ -31,22 +25,9 @@ class Game():
 
     def __updateObjects(self):
         pressedKeys = pygame.key.get_pressed()
-        #deadArrows = []
-        #for arrow in self.arrows:
-        #    deadArrow = arrow.update(pressedKeys, deadArrows)
-        #    if deadArrow:
-        #        deadArrows.append(deadArrow)
-        #for score in self.scores:
-        #    score.update(deadArrows)
         self.stateStack[-1].updateObjects(pressedKeys)
 
     def __updateScreen(self):
-        #self.screen.fill((0, 0, 0))
-        #for arrow in self.arrows:
-        #    arrow.draw(self.screen)
-        #for score in self.scores:
-        #    score.draw(self.screen)
-        #pygame.display.update()
         self.stateStack[-1].updateScreen()
 
     def __loadStates(self):
@@ -61,15 +42,6 @@ class Game():
             self.clock.tick(self.fps)
 
         pygame.quit()
-
-# players list
-# the index represents the player number
-# the string represents the player name
-# the number 1 or 0 represents if the player is playing
-# on this machine or not respectively
-players = [['Player 1', 0], ['Player 2', 1]]
-
-# arrowData = [direction, arrive time, speed] 
 
 game = Game()
 
